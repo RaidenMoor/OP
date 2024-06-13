3bc66aa55cc99912280da8ac85c1b7b33ff97021
Maria <159527845+RaidenMoor@users.noreply.github.com>
2024-06-13 09:26:42 +0400

    Create IVectorable.cs

---
+++ LR8/LR8/IVectorable.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LR8
+{
+    public interface IVectorable
+    {
+        double this[int index] { get; set; }
+        int Length { get; }
+        double GetNorm();
+        int CompareTo(object obj);
+    }
+}
