7bb335b778b258c4f34a792876c6bdd438d7ee81
Maria <159527845+RaidenMoor@users.noreply.github.com>
2024-06-13 09:26:13 +0400

    Create CreateLinkedListVectorForm.cs

---
+++ LR8/LR8/CreateLinkedListVectorForm.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace LR8
+{
+    public partial class CreateLinkedListVectorForm : Form
+    {
+        Form2 menu;
+        public CreateLinkedListVectorForm()
+        {
+            InitializeComponent();
+        }
+        public CreateLinkedListVectorForm(Form2 menu)
+        {
+            InitializeComponent();
+            this.menu = menu;
+        }
+        private void button3_Click(object sender, EventArgs e)
+        {
+            LinkedListVector vector = new LinkedListVector(Convert.ToInt32(numericUpDown1.Value));
+            try
+            {
+                double[] user_values = (textBox1.Text).Split(' ').Select(double.Parse).ToArray();
+                if (vector.Length != user_values.Length)
+                {
+                    string message = "Вы ввели недопустимы вектор! Повторите ввод!";
+                    MessageBox.Show(message);
+
+                }
+                else
+                {
+                    for (int i = 0; i < vector.Length; i++)
+                    {
+                        vector[i] = user_values[i];
+                    }
+
+                    LinkedListVectorForm vector_form = new LinkedListVectorForm(menu, vector);
+                    vector_form.Show();
+                    this.Close();
+                }
+            }
+            catch
+            {
+                string message = "Вы ввели недопустимы вектор! Повторите ввод!";
+                MessageBox.Show(message);
+            }
+        }
+        public void Create()
+        {
+            LinkedListVector vector = new LinkedListVector(Convert.ToInt32(numericUpDown1.Value));
+            try
+            {
+                double[] user_values = (textBox1.Text).Split(' ').Select(double.Parse).ToArray();
+                if (vector.Length != user_values.Length)
+                {
+                    string message = "Вы ввели недопустимы вектор! Повторите ввод!";
+                    MessageBox.Show(message);
+
+                }
+                else
+                {
+                    for(int i =0; i < vector.Length;i++)
+                    {
+                        vector[i] = user_values[i];
+                    }
+                    
+                    LinkedListVectorForm vector_form = new LinkedListVectorForm(menu, vector);
+                    vector_form.Show();
+                    vector_form.Focus();
+                    this.Close();
+                }
+            }
+            catch
+            {
+                string message = "Вы ввели недопустимы вектор! Повторите ввод!";
+                MessageBox.Show(message);
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            menu.Show();
+            menu.Focus();
+            this.Close();
+        }
+
+        private void CreateLinkedListVectorForm_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
